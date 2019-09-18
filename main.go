aeb3253beee730dfdf75ed890a652ef63e5aedf3
Philip Krause <pkrause@FL01MB5877.local>
2019-09-18 18:01:46 -0400

    firstcommit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var ai int32 = 50
+	var max int32 = 100
+	var min int32 = 0
+	var message = fmt.Sprintf("Is your number %d Enter higher, lower, or yes\n", AI)
+
+	input := ""
+
+	fmt.Println("The Number Guesser in GO!")
+	fmt.Println(message)
+
+	for input != "yes" {
+
+		mem = message
+		fmt.Scanln(&input)
+		fmt.Println(message)
+
+		if input == "higher" {
+			min = ai
+			ai = (max + min) / 2
+			fmt.Println(ai)
+		} else if input == "lower" {
+			max = ai
+			ai = (max + min) / 2
+		} else {
+			fmt.Println("A.I. wins again")
+		}
+	}
+
+}
